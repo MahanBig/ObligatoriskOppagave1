2978ec28ffe4d59f6f7047244b73d5ba3d849a39
Mahan <51638793+MahanBig@users.noreply.github.com>
2026-04-19 23:01:57 +0200

    Fixed

--- ObligatoriskOppgaveTest/Test1.cs
+++ ObligatoriskOppgaveTest/Test1.cs
@@ -11,12 +11,12 @@ namespace ObligatoriskOppgaveTest
         public void LoggInn_MedGyldigInfo_SkalReturnereBruker()
         {
             // Arrange
-            var uni = new Universitet();
+            Universitet uni = new Universitet();
             string brukernavn = "ola1";
             string passord = "pass123";
 
             // Act
-            var bruker = uni.LoggInn(brukernavn, passord);
+            Bruker? bruker = uni.LoggInn(brukernavn, passord);
 
             // Assert
             Assert.IsNotNull(bruker, "Innlogging feilet med gyldig legitimasjon.");
@@ -27,14 +27,14 @@ namespace ObligatoriskOppgaveTest
         public void MeldStudentPåKurs_GyldigKurs_SkalLeggeTilStudentIKurset()
         {
             // Arrange
-            var uni = new Universitet();
+            Universitet uni = new Universitet();
             uni.OprettKurs("IT101", "Programmering 1", 10, 30);
-            int studentId = 1; // Ola Nordmann fra konstruktøren
+            int studentId = 1;
 
             // Act
             uni.MeldStudentPåKurs(studentId, "IT101");
-            var kurs = uni.GetKursFraListe("IT101");
-            var student = uni.GetStudentFraListe(studentId);
+            Kurs? kurs = uni.GetKursFraListe("IT101");
+            Student? student = uni.GetStudentFraListe(studentId);
 
             // Assert
             Assert.IsTrue(kurs.Deltagere.Any(s => s.Id == studentId), "Studenten ble ikke lagt til i kurslisten.");
@@ -45,7 +45,7 @@ namespace ObligatoriskOppgaveTest
         public void RegistrerNyStudent_SjekkOmBrukerenBleLuggetTilIListe_SkalKasteException()
         {
             // Arrange
-            var uni = new Universitet();
+            Universitet uni = new Universitet();
 
             // Act
             uni.RegistrerNyStudent("Test Navn", "test@uni.no", "ola32", "passord123");
